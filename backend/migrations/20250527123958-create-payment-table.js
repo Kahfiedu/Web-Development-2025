1e3317b291b524672c37ccc5cb0a845a40df2d28
RaihanPrasetia <raihanprasetia86@gmail.com>
2025-05-27 19:54:27 +0700

    menambahkan table dan model payment

---
+++ backend/migrations/20250527123958-create-payment-table.js
@@ -0,0 +1,99 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('payments', {
+      id: {
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+        type: Sequelize.STRING(36),
+        defaultValue: Sequelize.UUIDV4,
+      },
+      userId: {
+        type: Sequelize.STRING(36),
+        defaultValue: Sequelize.UUIDV4,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      classId: {
+        type: Sequelize.STRING(36),
+        defaultValue: Sequelize.UUIDV4,
+        references: {
+          model: 'classes',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      amount: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM("pending", "completed", "failed"),
+        defaultValue: "pending",
+        allowNull: false
+      },
+      method_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      bank_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      no_rekening: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      atas_nama: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      payment_proof: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      payment_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      confirmation_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      confirmation_by: {
+        type: Sequelize.STRING(36),
+        defaultValue: Sequelize.UUIDV4,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('payments');
+  }
+};
